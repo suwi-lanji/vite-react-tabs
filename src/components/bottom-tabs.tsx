ff7a8aa558f1af15f9793322ac0908abe8777743
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-01-16 17:34:02 +0200

    add configured href to bottomtabs component

--- src/components/bottom-tabs.tsx
+++ src/components/bottom-tabs.tsx
@@ -5,7 +5,7 @@ export default function BottomTabs() {
     return (
         <div className="flex items-center justify-around py-2 sticky bottom-0 bg-opacity-85">
             { nav_items.map(item => 
-                <Link to={''} className="flex flex-col items-center">
+                <Link to={item.href} className="flex flex-col items-center">
                     <item.icon/>
                     <span className="text-xs font-semibold">{item.label}</span>
                 </Link>
