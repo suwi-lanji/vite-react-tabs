0b2ea44aab63d0c490bf4cad702178405cfd1693
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-01-16 17:33:18 +0200

    Add href to nav items configuration

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,6 +1,7 @@
 import { Home, Inbox, Search, UserRound } from "lucide-react"
 export interface BottomTabItem {
     id: number,
+    href: string,
     label: string | null,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     icon: any
@@ -9,21 +10,25 @@ export interface BottomTabItem {
 export const nav_items: BottomTabItem[] = [
 {
     id: 1,
+    href: "/",
     label: "Home",
     icon: Home
 },
 {
     id: 2,
+    href: "/search",
     label: "Search",
     icon: Search
 },
 {
     id: 3,
+    href: "/inbox",
     label: "Inbox",
     icon: Inbox
 },
 {
     id: 4,
+    href: "/profile",
     label: "Profile",
     icon: UserRound
 },
